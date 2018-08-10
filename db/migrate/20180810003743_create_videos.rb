bc3a294cbdf7dc9b6eff9ac276b6a109daf8edb5
alexheil <aheil4250@gmail.com>
2018-08-09 20:56:46 -0400

    migrate models

--- db/migrate/20180810003743_create_videos.rb
+++ db/migrate/20180810003743_create_videos.rb
@@ -1,6 +1,14 @@
 class CreateVideos < ActiveRecord::Migration[5.0]
   def change
     create_table :videos do |t|
+      t.references :user
+      t.string :title
+      t.string :video_data
+      t.string :image_data
+      t.text :description
+      t.integer :votes
+      t.datetime :start_time
+      t.string :slug
 
       t.timestamps
     end
--- db/migrate/20180810003750_create_votes.rb
+++ db/migrate/20180810003750_create_votes.rb
@@ -1,6 +1,8 @@
 class CreateVotes < ActiveRecord::Migration[5.0]
   def change
     create_table :votes do |t|
+      t.references :user
+      t.references :video
 
       t.timestamps
     end
